12b84cbe2bdc30a6be8ccd577076e576bddd9588
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-05-09 17:12:06 +0300

    create Spring-Discovery-Service project

--- Spring-Discovery-Service/src/main/java/ru/sergeypyzin/Spring/Discovery/Service/SpringDiscoveryServiceApplication.java
+++ Spring-Discovery-Service/src/main/java/ru/sergeypyzin/Spring/Discovery/Service/SpringDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package ru.sergeypyzin.Spring.Discovery.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
